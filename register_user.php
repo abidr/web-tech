c7867613c5bc02abf2f3779f2662cb9ba28708cf
EbnulAhsan <140329269+EbnulAhsan@users.noreply.github.com>
2025-03-15 20:00:28 +0600

    added user registration

--- register_user.php
+++ register_user.php
@@ -1,9 +1,64 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Document</title>
-  </head>
-  <body>
-    
-  </body>
-</html>
+<head>
+   
+</head>
+<body>
+    <header>
+    <h1>Registration for User </h1>
+    </header>
+    <main>
+            <form action="action.php" method="POST">
+                <fieldset>
+                    <legend>
+                        <h3>User Info: </h3>
+                    </legend>
+                    <table>
+                        <tr>
+                          <td>First-Name:</td>
+                          <td><input type="text" name="" id="First-Name"></td>
+                          </tr>
+                          <tr> 
+                            <td>Last-Name:</td>
+                            <td><input type="text" name="" id="Last-Name"></td>
+                        </tr>
+                        <tr>
+        <td>Password:</td>
+        <td>
+            <input type="password" name="" id="password">
+        </td>
+                        </tr>
+                        
+                        <tr>
+                            <td>Email Address:
+                            </td>
+                            <td>
+                              <input type="email" name="" id="email">
+                            </td>
+                            <tr>
+                                <td>Date of Birth:</td>
+                                <td><input type="email" name="" id="Date of Birth"></td>
+                            </tr>
+                            <tr>
+                                <td>Gender:</td>
+                                <td><input type="radio" name="gender" id="male">Male <input type="radio" name="gender" id="female">Female</td>
+                               
+                            </tr>
+                            <tr>
+                               <td> <input type="checkbox" name="" id="">I agree to all terms and condition.</td>
+                            </tr>
+                            
+                            <tr>
+                                <td>
+                                    <input type="button" value="Register">
+                                </td>
+                            </tr>
+                        </tr>
+            </fieldset>
+            </form>
+           
+               
+    </main>
+   
+   
+</body>
