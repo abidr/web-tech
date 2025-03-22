31683369a7d244917ff4aae06b9460e889f98962
Abidur Rahman <abid.nnt.g@gmail.com>
2025-03-22 23:12:23 +0600

    CSS Task 2

---
+++ css_task2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>CSS Task</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="box special2">
+      <h1>Hello there</h1>
+      <p>This is a random text</p>
+    </div>
+    <div class="box">
+      <h1>Hello there</h1>
+      <p>This is a random text</p>
+    </div>
+    <div class="box">
+      <h1>Hello there</h1>
+      <p>This is a random text</p>
+    </div>
+    <div class="box scrollable-box">
+      <h1 id="special">Hello there</h1>
+      <div class="element-top">
+        <p>This is a text</p>
+      </div>
+      <p>This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text.This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text. This is a random text.  </p>
+    </div>
+  </body>
+</html>
